b08739ab538fd84398e71d3d293ff1dad70467ab
Nathan Carroll <omnisoldat@live.com>
2016-05-02 15:33:15 -0500

    took out the custom attributes.

--- SimpleSalesForce/Attributes/CreateableAttribute.cs
+++ SimpleSalesForce/Attributes/CreateableAttribute.cs
@@ -1,15 +1,15 @@
-using System;
+//using System;
 
-namespace Simple.Salesforce
-{
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    public class CreateableAttribute : Attribute
-    {
-        public bool Createable { get; private set; }
+//namespace Simple.Salesforce
+//{
+//    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+//    public class CreateableAttribute : Attribute
+//    {
+//        public bool Createable { get; private set; }
 
-        public CreateableAttribute(bool createable)
-        {
-            Createable = createable;
-        }
-    }
-}
+//        public CreateableAttribute(bool createable)
+//        {
+//            Createable = createable;
+//        }
+//    }
+//}
--- SimpleSalesForce/Attributes/UpdateableAttribute.cs
+++ SimpleSalesForce/Attributes/UpdateableAttribute.cs
@@ -1,15 +1,15 @@
-using System;
+//using System;
 
-namespace Simple.Salesforce
-{
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    public class UpdateableAttribute : Attribute
-    {
-        public bool Updateable { get; private set; }
+//namespace Simple.Salesforce
+//{
+//    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+//    public class UpdateableAttribute : Attribute
+//    {
+//        public bool Updateable { get; private set; }
 
-        public UpdateableAttribute(bool updateable)
-        {
-            Updateable = updateable;
-        }
-    }
-}
+//        public UpdateableAttribute(bool updateable)
+//        {
+//            Updateable = updateable;
+//        }
+//    }
+//}
